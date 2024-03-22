7bd1a0aa4ab7790d1b7a962b1da2c5e0fecf2722
25dlee <136829451+25dlee@users.noreply.github.com>
2024-03-22 23:12:51 +0100

    camera set up

---
+++ RedRidinghood/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    
+    public static CameraManager Instance { get; private set; }
+    public enum CameraMode { ThirdPerson, FirstPerson }
+    public CameraMode cameraMode;
+    public GameObject thirdPOVCamera;
+    public GameObject firstPOVCamera;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        ChangeCameraMode(cameraMode);
+    }
+
+    private void OnValidate()
+    {
+        ChangeCameraMode(cameraMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeCameraMode(CameraMode.ThirdPerson);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeCameraMode(CameraMode.FirstPerson);
+        }
+        
+    }
+
+    void ChangeCameraMode(CameraMode mode)
+    {
+        cameraMode = mode;
+        thirdPOVCamera.SetActive(false);
+        firstPOVCamera.SetActive(false);
+
+        switch (cameraMode)
+        {
+            case CameraMode.ThirdPerson:
+                thirdPOVCamera.SetActive(true);
+                break;
+            case CameraMode.FirstPerson:
+                firstPOVCamera.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
